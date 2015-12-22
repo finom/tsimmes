5f89c035265d28e4c26c744c99e7f5dcbc39ad21
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2015-12-22 16:12:06 +0200

    fixed bugs, added tests

--- umd/bala.umd.js
+++ umd/bala.umd.js
@@ -1,24 +1,28 @@
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
-    } else {
-        root.$ = factory();
-  }
-}(this, function () {
-    return (function(document, fn, $) {
-      $ = function(s, context) {
-	    fn.push.apply( this, !s ? fn : s.nodeType || s == window ? [s] : "" + s === s ? /</.test( s ) 
-	    ? ( ( i = document.createElement( context || 'q' ) ).innerHTML = s, i.children ) : (context&&$(context)[0]||document).querySelectorAll(s) : /f/.test(typeof s) ? /c/.test(document.readyState) ? s() : document.addEventListener('DOMContentLoaded', s) : s );
-      }; 
-    $.fn = $.prototype = fn;
+(function(root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define([], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		module.exports = factory();
+	} else {
+		root.$ = factory();
+	}
+}(this, function() {
+	return (function(document, fn, $, Bala, i) {
+		$ = function(s, context) {
+			return new Bala(s, context);
+		};
+		Bala = function(s, context) {
+			fn.push.apply(this, !s ? fn : s.nodeType || s == window ? [s] : "" + s === s ? /</.test(s) ? ((i = document.createElement(context || 'q')).innerHTML = s, i.children) : (context && $(context)[0] || document).querySelectorAll(s) : /f/.test(typeof s) ? /c/.test(document.readyState) ? s() : document.addEventListener('DOMContentLoaded', s) : s);
+			return this;
+		};
 
-    $.one = function(s) {
-      return $(s, context)[0]||null;
-    };
+		$.fn = Bala.prototype = fn;
 
-    return $;
-      
-    })(document, []);
+		$.one = function(s, context) {
+			return $(s, context)[0] || null;
+		};
+
+		return $;
+
+	})(document, []);
 }));
