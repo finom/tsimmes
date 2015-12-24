8acd8f9dfcdef62f1612a1d0a3c1b4de6b1c69c9
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2015-12-24 14:32:23 +0200

    improved comments

--- umd/bala.umd.js
+++ umd/bala.umd.js
@@ -13,23 +13,24 @@
 		};
 
 		Bala = function(s, context) {
-			fn.push.apply(this, !s
-				? fn // if arg is falsy, pass []
+			fn.push.apply(this, !s // if arg is falsy
+				? fn // then pass []
 				: s[s_addEventListener] // else if arg is node or window,
-					? [s] // pass [s]
-					: "" + s === s // else if arg is string
-						? /</.test(s) // if contains "<" (if HTML is passed)
-							? ((context = document.createElement(context || 'q')).innerHTML = s, context.children) // parse it and return node.children
-							: context // if context is truly
+					? [s] // then pass [s]
+					: "" + s === s // else if arg is a string
+						? /</.test(s) // if the string contains "<" (if HTML code is passed)
+							// then parse it and return node.children
+							? ((context = document.createElement(context || 'q')).innerHTML = s, context.children)
+							: context // else if context is truly
 								? ((context = $(context)[0]) // if context element is found
-									? context[s_querySelectorAll](s) // select element from context
-									: fn) // if context isn't found, pass []
+									? context[s_querySelectorAll](s) // then select element from context
+									: fn) // else pass [] (context isn't found)
 								: document[s_querySelectorAll](s) // else select elements globally
 						: /f/.test(typeof s) // else if function is passed
 							? document.readyState[0] == 'c' // if DOM is ready
-								? s() // run function
+								? s() // then run given function
 								: document[s_addEventListener]('DOMContentLoaded', s) // else wait for DOM ready
-							: s); // else guessing that s is array-like object
+							: s); // else guessing that s variable is array-like object
 		};
 
 		$.fn = Bala.prototype = fn;
