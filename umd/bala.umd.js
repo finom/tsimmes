ec9d62913ec13ca1cb1f74dd4904cde0ea29988b
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2015-12-27 23:11:17 +0200

    save 3 bytes

--- umd/bala.umd.js
+++ umd/bala.umd.js
@@ -7,15 +7,14 @@
 		root.$ = factory();
 	}
 }(this, function() {
-	return (function(document, fn, s_addEventListener, s_querySelectorAll, $, Bala) {
+	return (function(document, s_addEventListener, s_querySelectorAll, $, Bala) {
 		$ = function(s, context) {
 			return new Bala(s, context);
 		};
 
 		Bala = function(s, context) {
-			fn.push.apply(this, !s // if arg is falsy
-				? fn // then pass []
-				: s[s_addEventListener] // else if arg is node or window,
+			s && [].push.apply(this, // if s is truly then push the following
+				s[s_addEventListener] // if arg is node or window,
 					? [s] // then pass [s]
 					: "" + s === s // else if arg is a string
 						? /</.test(s) // if the string contains "<" (if HTML code is passed)
@@ -24,7 +23,7 @@
 							: context // else if context is truly
 								? ((context = $(context)[0]) // if context element is found
 									? context[s_querySelectorAll](s) // then select element from context
-									: fn) // else pass [] (context isn't found)
+									: []) // else pass [] (context isn't found)
 								: document[s_querySelectorAll](s) // else select elements globally
 						: /f/.test(typeof s) // else if function is passed
 							? document.readyState[0] == 'c' // if DOM is ready
@@ -33,13 +32,14 @@
 							: s); // else guessing that s variable is array-like object
 		};
 
-		$.fn = Bala.prototype = fn;
+		$.fn = Bala.prototype = [];
 
 		$.one = function(s, context) {
 			return $(s, context)[0] || null;
 		};
 
 		return $;
-	})(document, [], 'addEventListener', 'querySelectorAll');
+	})(document, 'addEventListener', 'querySelectorAll');
+
 
 }));
