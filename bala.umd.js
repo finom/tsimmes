dfc4e5da2e1fc8e51621af7794c53f13f6479f86
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2017-03-16 15:04:20 +0200

    Accept symbol, number and boolean (donot throw errors)

--- bala.umd.js
+++ bala.umd.js
@@ -1,12 +1,12 @@
 (function(root, $) {
-	$ = ((document, s_querySelectorAll, $) => (
+	$ = ((document, s_querySelectorAll, fn, $) => (
 	($ = (s, context, bala) => (
-		(bala = Object.create($.fn)),
+		(bala = Object.create(fn)),
 
 		s && bala.push( // if s is truly then push the following
 			...(s.dispatchEvent // if arg is node or window,
 				? [s] // then pass [s]
-				: "" + s === s // else if arg is a string
+				: typeof s == 'string' // else if arg is a string
 					? /</.test(s) // if the string contains "<" (if HTML code is passed)
 						// then parse it and return node.children
 						// use 'querySelectorAll' (HTMLUnknownElement) if content is not presented
@@ -17,16 +17,16 @@
 								? context[s_querySelectorAll](s) // then select element from context
 								: bala) // else pass [] (context isn't found)
 							: document[s_querySelectorAll](s) // else select elements globally
-					: s)), // else guessing that s variable is array-like Object
+					: s.length ? s : fn)), // else guessing that s variable is array-like Object
         bala
 	)),
 
-	($.fn = []),
+	($.fn = fn),
 
 	($.one = (s, context) => $(s, context)[0]),
 
 	$
-))(document, 'querySelectorAll');
+))(document, 'querySelectorAll', []);
 ;
 
 
--- tools/generateUmd.js
+++ tools/generateUmd.js
@@ -3,6 +3,6 @@ const path = require('path');
 
 const umdTemplate = fs.readFileSync(path.join(__dirname, './umdTemplate.js'), { encoding: 'utf-8' });
 const bala = fs.readFileSync(path.join(__dirname, '../src.js'), { encoding: 'utf-8' });
-const balaUmd = umdTemplate.replace('BALA_PLACEHODER', bala)
+const balaUmd = umdTemplate.replace('BALA_PLACEHODER', bala);
 
 fs.writeFileSync(path.join(__dirname, '../bala.umd.js'), balaUmd);
